4d4ba8d7b01b719a933dddb8b14af23a8f4e963f
IsHYuhi <yuhiregend@icloud.com>
2020-05-02 14:11:41 +0900

    add autoaugment

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 net = models.resnet101(pretrained=False, num_classes=8)
+#net = models.resnet152(pretrained=False, num_classes=8)
 
 net = net.to(device)
 
@@ -105,7 +106,7 @@ def plot_log(types, data):
     plt.savefig('./result/'+ types +'.png')
 
 def main():
-    size = 256
+    size = 32
     mean = (0.485, 0.456, 0.406)
     std = (0.229, 0.224, 0.225)
     train_list, val_list = make_datapath_list(phase="train", rate=0.9)
